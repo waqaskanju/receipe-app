bfcf2357329b0cf9f2797be11c9c1cc639a69d2f
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-03 17:12:58 -0500

    Run linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users, path: ""
-    resources :users, only: [:index, :show] do
-      resources :foods, only: [:index, :show, :destroy]
-      resources :recipes, only: [:index, :show, :destroy] 
-      resources :recipe_foods
-    end
+  devise_for :users, path: ''
+  resources :users, only: %i[index show] do
+    resources :foods, only: %i[index show destroy]
+    resources :recipes, only: %i[index show destroy]
+    resources :recipe_foods
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root 'users#index'
 end
