71702391582179917e61b38edcc74159f640e0eb
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-03 17:11:48 -0500

    Run linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, path: ""
-    resources :users only: [:index, :show] do
-      resources :foods only: [:index, :show, :destroy]
-      resources :recipes only: [:index, :show, :destroy] 
+    resources :users, only: [:index, :show] do
+      resources :foods, only: [:index, :show, :destroy]
+      resources :recipes, only: [:index, :show, :destroy] 
       resources :recipe_foods
-
-    
     end
-
-
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
