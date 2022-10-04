8deb273423d3fbd65b836983918e9cde6b7fb060
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-04 14:47:21 -0500

    Create seeds file to populate db faster

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.first
+
+recipe1 = Recipe.create!(author: user1, name: 'Pizza', preparation_time: 10, cooking_time: 15, description: 'Italian food', public: true)
+recipe2 = Recipe.create!(author: user1, name: 'Burger', preparation_time: 10, cooking_time: 15, description: 'Fast food', public: true)
+recipe3 = Recipe.create!(author: user1, name: 'Bandeja Paisa', preparation_time: 10, cooking_time: 15, description: 'Colombian food', public: true)
