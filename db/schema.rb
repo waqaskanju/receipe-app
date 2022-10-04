ac9e697a3ee6d8fbba5c71f439f338cc974dac18
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-04 14:46:00 -0500

    Add user references as required and update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_220109) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_185351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,10 +23,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_220109) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "public_recipes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipe_id", null: false
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -37,6 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_220109) do
     t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_220109) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipes", "users"
 end
