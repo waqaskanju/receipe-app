8b2cbb6d2a268b3c50796952d857786e6000ee2d
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-04 17:33:46 -0500

    Add associations to RecipeFood model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class RecipeFood < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :food
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
 end
