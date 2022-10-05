099fff0269e0c089ad61d1cff34e736ccd9f88e4
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-05 17:45:26 -0500

    Create public_recipes view

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -3,6 +3,6 @@ class PublicRecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.where(public: true).order(created_at: :desc)
+    @recipes = Recipe.where(public: true).order(created_at: :asc)
   end
 end
