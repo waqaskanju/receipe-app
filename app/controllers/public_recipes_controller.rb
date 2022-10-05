16eed4862311e71a92f1c4691b1b076e49ea7e7f
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-05 16:49:47 -0500

    Create public_recipes controller

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,8 @@
+class PublicRecipesController < ApplicationController
+  before_action :set_recipe, only: %i[show edit update destroy]
+
+  # GET /recipes or /recipes.json
+  def index
+    @recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
+end
