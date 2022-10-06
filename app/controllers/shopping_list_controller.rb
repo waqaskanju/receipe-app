5c6fa20652223d4d71585d9b3ca95a5f411e34bf
waqas ahmad <waqaskanju@gmail.com>
2022-10-06 17:38:33 +0500

    add application helper class

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class ShoppingListController < ApplicationController
-    def index
-      @ingredients = RecipeFood.includes(:food).where(recipe: Recipe.where(user: current_user))
-    end
+  def index
+    @ingredients = RecipeFood.includes(:food).where(recipe: Recipe.where(user: current_user))
   end
-  
+end
